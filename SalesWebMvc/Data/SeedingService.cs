c7b9ac741dc48a9951c3c4bee397d0585a8e1f94
Gilberto Espinosa <gilbertosilvaespinosa@gmail.com>
2023-01-12 16:35:04 -0300

    Implementing simple search

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using SalesWebMvc.Models.Enums;
-using System.Threading.Tasks;
 using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Data
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -45,6 +42,7 @@ namespace SalesWebMvc
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
