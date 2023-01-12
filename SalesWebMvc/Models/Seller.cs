236b90a036913dbfa63174501986080ead3e9180
Gilberto Espinosa <gilbertosilvaespinosa@gmail.com>
2023-01-12 12:21:47 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,17 +8,25 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Required {0}")]
+        [StringLength(60, MinimumLength = 4, ErrorMessage = "Unsupported {0} length.")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Required {0}")]
+        [EmailAddress(ErrorMessage = "Enter a valid email.")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Display(Name="Birth Date")]
+        [Required(ErrorMessage = "Required {0}")]
+        [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
-        [Display(Name ="Base Salary")]
+        [Required(ErrorMessage = "Required {0}")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
+        [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
